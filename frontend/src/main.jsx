273cb2f33c057a93fc2bb59cf41a73a365dea5b2
prar1907 <prarthana358@gmail.com>
2026-04-26 02:57:36 -0700

    final v1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, useNavigate } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
 import "./styles/index.css";
 
+function Auth0ProviderWithNavigate() {
+  const navigate = useNavigate();
+  return (
+    <Auth0Provider
+      domain="dev-par7l5f3dh7tpjfr.us.auth0.com"
+      clientId="bR8KAUF3CRzsWHGFn8kgTanpZuBgDU9A"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+      onRedirectCallback={() => {
+        const returnTo = localStorage.getItem("auth_redirect") || "/doctor";
+        localStorage.removeItem("auth_redirect");
+        navigate(returnTo, { replace: true });
+      }}
+      skipRedirectCallback={!window.location.search.includes("code=")}
+    >
+      <App />
+    </Auth0Provider>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Auth0ProviderWithNavigate />
+    </BrowserRouter>
   </React.StrictMode>
 );
